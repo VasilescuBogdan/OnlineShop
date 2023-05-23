f3d605f8d2cd3cb4d551f5686ff31c08a70ff4c3
Teodor Bogdan Vasilescu <vasilescubogdan1@gmail.com>
2023-05-23 17:43:07 +0300

    authentication frontend part 2

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AdminComponent } from './admin/admin.component';
-import { UserComponent } from './user/user.component';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { ForbiddenComponent } from './forbidden/forbidden.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {AdminComponent} from './admin/admin.component';
+import {UserComponent} from './user/user.component';
+import {LoginComponent} from './login/login.component';
+import {HeaderComponent} from './header/header.component';
+import {ForbiddenComponent} from './forbidden/forbidden.component';
 import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {RouterModule} from "@angular/router";
+import {AuthGuard} from "./_auth/auth.guard";
+import {AuthInterceptor} from "./_auth/auth.interceptor";
+import {UserService} from "./_services/user.service";
 
 @NgModule({
   declarations: [
@@ -30,7 +33,16 @@ import {RouterModule} from "@angular/router";
     HttpClientModule,
     RouterModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- Frontend/src/app/user/user.component.ts
+++ Frontend/src/app/user/user.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../_services/user.service";
+import {lastValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
 
+  message: any;
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.forUser();
+  }
+
+  async forUser() {
+    try {
+      const response:any = await lastValueFrom(this.userService.forUser());
+      console.log(response);
+      this.message = response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
