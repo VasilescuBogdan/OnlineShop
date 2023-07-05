62170011c6d559f34b5dcafe747ba697b361eafa
Teodor Bogdan Vasilescu <vasilescubogdan1@gmail.com>
2023-07-05 16:21:50 +0300

    t

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+    title = 'Online Shop';
 }
